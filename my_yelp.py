f5ba17d0578ee9b8fc96c901f80fe3d331c52812
Kumail Razvi <alikabanda@gmail.com>
2018-08-25 12:29:23 -0500

    Working REST API call

---
+++ my_yelp.py
@@ -0,0 +1,18 @@
+import requests
+api_key = "e6mtc6SVQPPS68uiYK6KNaYm4Mit_SChSxouObNX7QHmh-eZi3Y2NGE_1D3kPOC7PZwKslLfTeMpQpsF0dNftkSxDJLiaZOToOlMheJS7p5ljt161KBEdO0tC2t_W3Yx"
+
+url = "https://api.yelp.com/v3/businesses/search"
+
+my_headers = {
+    "Authorization": "Bearer %s" % api_key
+}
+
+my_params = {
+    "term": "restaurants",
+    "location": "chicago",
+    "limit": 3,
+}
+
+businesses_object = requests.get(url, headers=my_headers, params=my_params)
+businesses_dict = businesses_object.text
+print(businesses_dict)
